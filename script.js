aa9149f0b42fb65889720e9fcd78d7fbd35dd212
Emikot123 <emil.asadullayev20@gmail.com>
2026-07-18 21:06:17 +0400

    A lot of changes version 2.0 released

--- script.js
+++ script.js
@@ -15,11 +15,13 @@ let followers = document.getElementById("followers");
 let following = document.getElementById("following");
 let email = document.getElementById("email");
 let search = document.getElementById("search");
+let pfp = document.getElementById("pfp_icon");
 
 button.onclick = () => {
     let choice = search.value;
     getData(choice).then(function(response){
         name_user.textContent = response.login;
+        username.href = response.html_url;
         username.textContent = response.name;
         if(response.company !== null){
             company.textContent = response.company;
@@ -34,6 +36,56 @@ button.onclick = () => {
         } else {
             email.textContent = "Not Shown";
         }
+        pfp.src = response.avatar_url;
+        getRepoData(choice)
 
     })
+
+}
+
+async function getRepoData(user){
+    let response = await fetch(`https://api.github.com/users/${user}/repos`);
+    let data = await response.json();
+    console.log(data);
+    display(data);
+    return data;
+}
+
+
+let visibility = document.getElementById("visibility");
+let repo_NameLink = document.getElementById("repo_name");
+let count = document.getElementById("repo_count");
+let program = document.getElementById("repo_lang");
+const reposContainer = document.querySelector("#repo_container");
+
+function display(repos) {
+    reposContainer.innerHTML = "";
+
+    repos.forEach(repo => {
+        const repoElement = document.createElement("article");
+
+        repoElement.classList.add("repo-card");
+
+        repoElement.innerHTML = `
+        <h2>
+        <a href="${repo.html_url}" target="_blank">
+          ${repo.name}
+        </a>
+        </h2>
+
+        <p>
+            Stars: ${repo.stargazers_count}
+        </p>
+
+      <p>
+        Visibility: ${repo.visibility}
+      </p>
+
+      <p>
+        Programming Language: ${repo.language ?? "Not shown"}
+      </p>
+    `;
+
+        reposContainer.appendChild(repoElement);
+    });
 }
